d6ee740a009e4771d34f1331a680078b76332606
JACUMA6 <J0s3cub432@gmail.com>
2025-03-31 00:12:44 -0600

    Initial project

---
+++ config-service/src/test/java/mcsv/izzi/config/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mcsv.izzi.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
