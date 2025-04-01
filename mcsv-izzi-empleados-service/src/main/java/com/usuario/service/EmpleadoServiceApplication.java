7dabc95c626f7e3bce3c88806aad3c17693b1bb0
JACUMA6 <J0s3cub432@gmail.com>
2025-03-31 19:52:15 -0600

    Update project

--- mcsv-izzi-empleados-service/src/main/java/com/usuario/service/EmpleadoServiceApplication.java
+++ mcsv-izzi-empleados-service/src/main/java/com/usuario/service/EmpleadoServiceApplication.java
@@ -3,9 +3,11 @@ package com.usuario.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmpleadoServiceApplication {
 
 	public static void main(String[] args) {
--- mcsv-izzi-usuarios-service/src/main/java/mcsv/izzi/usuarios/service/UsuarioServiceApplication.java
+++ mcsv-izzi-usuarios-service/src/main/java/mcsv/izzi/usuarios/service/UsuarioServiceApplication.java
@@ -3,9 +3,11 @@ package mcsv.izzi.usuarios.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
