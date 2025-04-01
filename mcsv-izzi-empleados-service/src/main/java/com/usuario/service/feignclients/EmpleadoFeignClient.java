fb7df3316bb8e1eb47e45d84acec680916cd9383
JACUMA6 <J0s3cub432@gmail.com>
2025-03-31 21:43:19 -0600

    Update EmpleadoFeignClient.java

--- mcsv-izzi-empleados-service/src/main/java/com/usuario/service/feignclients/EmpleadoFeignClient.java
+++ mcsv-izzi-empleados-service/src/main/java/com/usuario/service/feignclients/EmpleadoFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "mcsv-izzi-usuarios-service",url = "http://localhost:8003")
+@FeignClient(name = "mcsv-izzi-usuarios-service")
 @RequestMapping("/users")
 public interface EmpleadoFeignClient {
 
