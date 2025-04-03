816746954ed34c4a6a065be5dfac7f20da9ea45a
JACUMA6 <J0s3cub432@gmail.com>
2025-04-02 21:32:57 -0600

    Update project

--- empleados-service/src/main/java/empleados/service/controlador/EmpleadoController.java
+++ empleados-service/src/main/java/empleados/service/controlador/EmpleadoController.java
@@ -3,7 +3,9 @@ package empleados.service.controlador;
 import empleados.service.entidades.Empleados;
 import empleados.service.modelos.Usuarios;
 import empleados.service.servicio.EmpleadoService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,16 +44,25 @@ public class EmpleadoController {
 		return ResponseEntity.ok(nuevoEmpleado);
 	}
 
+	@CircuitBreaker(name = "usuariosCB", fallbackMethod = "fallBackSaveUsers")
 	@PostMapping("/usuario/{usuarioId}")
 	public ResponseEntity<Usuarios> guardarUsers(@PathVariable("usuarioId") int usuarioId, @RequestBody Usuarios carro){
 		Usuarios nuevoCarro = service.saveCarro(usuarioId, carro);
 		return ResponseEntity.ok(nuevoCarro);
-	} 
+	}
 
-	
+	@CircuitBreaker(name = "allCB", fallbackMethod = "fallBackGetAll")
 	@GetMapping("/todos/{usuarioId}")
 	public ResponseEntity<Map<String, Object>> listarTodosLosUsuarios(@PathVariable("usuarioId") int usuarioId){
 		Map<String,Object> resultado = service.getUsuarioAndVehiculos(usuarioId);
 		return ResponseEntity.ok(resultado);
+	}
+
+	private ResponseEntity<Empleados> fallBackSaveUsers(@PathVariable("userId") int userId, @RequestBody Usuarios users, RuntimeException e) {
+		return new ResponseEntity("El Empleado " + userId + "  no tiene ningun usuario asociado", HttpStatus.OK);
+	}
+
+	public ResponseEntity<Map<String, Object>> fallBackGetAll(@PathVariable("userId") int userId, RuntimeException e) {
+		return new ResponseEntity("El Empleado " + userId + " tiene usuarios asociados", HttpStatus.OK);
 	}
 }
